79e47cacd41b6745571a53268031602147bb3268
thawancamara <thawancamara@gmail.com>
2023-11-20 09:47:51 -0300

    script

--- script.sh
+++ script.sh
@@ -16,3 +16,4 @@ sed -i "47 i $USER\tALL=(ALL:ALL) ALL" /etc/sudoers
 apt install -y curl
 
 curl -fsSL https://get.docker.com/ | sh
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
