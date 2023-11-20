0ad0cc654d2b81815e64d46f14b06b53f1492d34
thawancamara <thawancamara@gmail.com>
2023-11-20 10:10:07 -0300

    scripts update

--- script.sh
+++ script.sh
@@ -16,4 +16,3 @@ sed -i "47 i $USER\tALL=(ALL:ALL) ALL" /etc/sudoers
 apt install -y curl
 
 curl -fsSL https://get.docker.com/ | sh
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
