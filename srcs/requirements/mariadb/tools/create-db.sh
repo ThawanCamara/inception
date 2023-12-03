0d8ac3c4d16a93c020d823dcf0096aade96f0845
Thawan Camara <thawancamara@gmail.com>
2023-12-02 22:14:24 -0300

    help3

--- srcs/requirements/mariadb/tools/create-db.sh
+++ srcs/requirements/mariadb/tools/create-db.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE_NAME" ]; then
     mysqld_safe --datadir=/var/lib/mysql&
     sleep 1
 
-mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -e "CREATE DATABASE $MYSQL_DATABASE_NAME;"
 mysql -e "CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO $MYSQL_USER@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
